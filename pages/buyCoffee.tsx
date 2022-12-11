2175b61be05f0863b80c9f996cc0d6a909f93ab2
Pablo Lopez <pablo.lopez.santori@gmail.com>
2022-12-11 13:11:03 +0000

    Add success message

--- pages/buyCoffee.tsx
+++ pages/buyCoffee.tsx
@@ -7,6 +7,7 @@ export default function BuyCoffee() {
     const [contract, setContract] = useState(null);
     const [message, setMessage] = useState("");
     const [name, setName] = useState("");
+    const [coffePurchased, setCoffeePurchased] = useState(false);
 
     const contractAddress = "0x8349482D0E6527ad294068Af095262Fd1498df9e";
     const contractABI = abi.abi;
@@ -30,7 +31,7 @@ export default function BuyCoffee() {
             { gasLimit: 300000 }
         );
         await transaction.wait();
-        return transaction;
+        setCoffeePurchased(true);
     }
 
     useEffect(() => {
@@ -38,7 +39,12 @@ export default function BuyCoffee() {
     }, []);
 
     return (
-        <div className="flex flex-col p-12 shadow-md rounded-lg" >
+        <div className="flex flex-col p-12 shadow-md rounded-lg w-96" >
+            {coffePurchased && (
+                <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                    <span className="block sm:inline">Coffee purchased successfully!</span>
+                </div>
+            )}
             <h1 className="text-3xl font-bold mb-4">Buy Coffee</h1>
             <input
                 type="text"
@@ -53,8 +59,9 @@ export default function BuyCoffee() {
                 className="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm mb-6"
             />
             <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-500 disabled:cursor-not-allowed"
                 onClick={() => buyCoffee(contract, name, message)}
+                disabled={!!coffePurchased}
             >
                 Buy Coffee
             </button>
