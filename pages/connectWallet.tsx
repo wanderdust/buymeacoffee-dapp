515b7093c88df24ecc7bc53aa94dc115bf85afc0
Pablo Lopez <pablo.lopez.santori@gmail.com>
2022-12-13 13:29:58 +0000

    Add instructions

--- pages/connectWallet.tsx
+++ pages/connectWallet.tsx
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 
 export default function ConnectWallet() {
@@ -10,8 +10,22 @@ export default function ConnectWallet() {
         setWallet(accounts[0]);
     }
 
+    const card = (message: string) => {
+        return (
+            <div className="flex flex-row bg-gray-100 w-48 rounded m-6 items-center text-center">
+                <p className="p-3 font-normal">{message}</p>
+            </div>
+        )
+    }
+
     return (
-        <div>
+        <div className="flex items-center flex-col" >
+            <h1 className="text-3xl font-bold">How it Works</h1>
+            <div className="flex flex-row mb-8">
+                {card("1. Connect your Metamask Wallet")}
+                {card("2. Select how much coffee to buy")}
+                {card("3. Confirm transaction in your Wallet")}
+            </div>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 onClick={connectNewWallet}
             >
