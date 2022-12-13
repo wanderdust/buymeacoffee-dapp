5b0bb0fb95389f5931939e30f470bc775b7319fe
Pablo Lopez <pablo.lopez.santori@gmail.com>
2022-12-13 21:23:55 +0000

    Change color of error message

--- pages/connectWallet.tsx
+++ pages/connectWallet.tsx
@@ -32,7 +32,7 @@ export default function ConnectWallet() {
     return (
         <div className="flex items-center flex-col" >
             {!!error && (
-                <div className="bg-red-100 border border-red-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
                     <span className="block sm:inline">{error} Refresh the page and try again.</span>
                 </div>
             )}
