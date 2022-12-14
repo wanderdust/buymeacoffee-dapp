7652cfc01925fd8dcaad9f4813bfb7b403d30221
Pablo Lopez <pablo.lopez.santori@gmail.com>
2022-12-14 08:48:14 +0000

    Move banner to its own component

---
+++ pages/banner.tsx
@@ -0,0 +1,11 @@
+export default function Banner() {
+    return (
+        <div className="flex justify-center bg-yellow-400 w-full py-6 px-6 mb-10 md:mb-24">
+            <p>
+                This app only works using Goerli Ethereum,
+                a tesnet that is free to use. Don't try sending real Ethereum.
+                To get free Goerli Ethereum, visit <a className="underline" href="https://www.alchemy.com/overviews/goerli-faucet" target="_blank">Goerli Faucet</a>.
+            </p>
+        </div>
+    )
+}
