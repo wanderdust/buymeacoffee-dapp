ba3a3c087d68582aa1df110907df16efc4ea0ec6
Pablo Lopez <pablo.lopez.santori@gmail.com>
2022-12-14 09:12:58 +0000

    Style transaction list

--- pages/transactionList.tsx
+++ pages/transactionList.tsx
@@ -5,14 +5,13 @@ export default function TransactionList({ allCoffee = [] }: any) {
             <h1 className="text-l font-medium mb-2">Last Transactions </h1>
             <div className="flex flex-col">
                 {allCoffee.length > 0 && allCoffee.map((coffee: any) => (
-                    <div className="flex flex-row justify-between p-4 shadow-md rounded-lg mb-4 bg-sky-50">
+                    <div className="flex flex-row justify-between md:max-w-3xl p-4 shadow-md rounded-lg mb-4 bg-sky-50 hover:bg-sky-100 transition delay-75">
                         <div className="flex flex-col mr-6">
                             <p className="font-medium">Name: <span className="font-normal">{coffee.name}</span></p>
                             <p className="font-medium hidden md:block">Address: <span className="font-normal">{coffee.address}</span></p>
                             <p className="font-medium">Message: <span className="font-normal">{coffee.message}</span></p>
                         </div>
-                        <p className="hidden md:block">{coffee.timestamp.toDateString()}</p>
-                        <p className="md:hidden">{coffee.timestamp.toLocaleDateString()}</p>
+                        <p>{coffee.timestamp.toLocaleDateString()}</p>
                     </div>
                 ))}
             </div>
